aec29490df047bac8edca985a25d5a1f3fa0c7de
Ismaxis <max200412345@gmail.com>
2021-07-31 19:19:18 +0300

    New cool and full work collision

--- Main.cpp
+++ Main.cpp
@@ -10,32 +10,29 @@ int main()
 {	
 	// Window
 	const sf::Vector2i winSize = { 960, 544 };
-	sf::RenderWindow window(sf::VideoMode(winSize.x, winSize.y), "SFML window", sf::Style::Default);
+	sf::RenderWindow window(sf::VideoMode(winSize.x, winSize.y), "Platformer", sf::Style::None);
 	window.setFramerateLimit(60);
 
 	// Player
 	Player player("Textures/Player32X64.png");
 	player.setPos(sf::Vector2f(winSize.x / 10, winSize.y / 2));
-	sf::Vector2f curVel;
-	sf::Vector2f curPos;
-	sf::Vector2f nextPos;
 
 	//Map
 	Map map("Maps/map2.txt", "Maps/mapSheet.png", player.getSize());
 	std::vector<std::vector<sf::Sprite>> mapSprite;
-
+	
 	const int cageSize = map.getCageSize();
 	const sf::Vector2i gridSize = { winSize.x / cageSize, winSize.y / cageSize };
 
-	const sf::Vector2f defVel = { 5.0f, 15.0f };
-	float maxVel = cageSize;
-	sf::Vector2<sf::Vector2<bool>> isCollide = { {false, false}, {false, false} };
-
+	sf::Clock clock;
+	int lastDir = 0;
 	// Window loop
 	while (window.isOpen())
 	{
-		sf::Event event;
+		float time = clock.restart().asMilliseconds();
 
+		// Event loop
+		sf::Event event;
 		while (window.pollEvent(event))
 		{
 			if (event.type == sf::Event::Closed)
@@ -48,110 +45,35 @@ int main()
 				// X movement
 				if (event.key.code == sf::Keyboard::D)
 				{
-					curVel.x = defVel.x;
+					lastDir = 1;
 				}
 				else if (event.key.code == sf::Keyboard::A)
 				{
-					curVel.x = -defVel.x;
+					lastDir = -1;
 				}
 				
 				// Jump 
-				if ((event.key.code == sf::Keyboard::Space || event.key.code == sf::Keyboard::W) && curVel.y == 0)
+				if ((event.key.code == sf::Keyboard::Space || event.key.code == sf::Keyboard::W) && player.getStatus())
 				{
-					curVel.y = defVel.y;
+					player.Jump();
 				}
 			}
-			else if (!sf::Keyboard::isKeyPressed(sf::Keyboard::A) && !sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+			else if (!sf::Keyboard::isKeyPressed(sf::Keyboard::D) && !sf::Keyboard::isKeyPressed(sf::Keyboard::A))
 			{
-				curVel.x = 0;
-			}
-		}
-		
-		// Max velocity check
-		if (abs(curVel.x) > maxVel)
-		{
-			if (curVel.x > 0)
-			{
-				curVel.x = maxVel;
-			}
-			else if (curVel.x < 0)
-			{
-				curVel.x = -maxVel;
-			}
-		}
-		if (abs(curVel.y) > maxVel)
-		{
-			if (curVel.y > 0)
-			{
-				curVel.y = maxVel;
-			}
-			else if (curVel.y < 0)
-			{
-				curVel.y = -maxVel;
+				lastDir = 0;
 			}
 		}
 
-		player.setVel(curVel);
 
-		// Colllision check
-		// Window collide
-		curPos = player.getPos();
-		// X
-		if (curPos.x > winSize.x)
-		{
-			curVel.x = 0;
-			player.setPos(sf::Vector2f(winSize.x - 1, curPos.y));
-		}
-		else if (curPos.x < 0)
-		{
-			curVel.x = 0;
-			player.setPos(sf::Vector2f(1, curPos.y));
-		}
-		// Y
-		if (curPos.y < winSize.y && !isCollide.y.x)
-		{
-			curVel.y -= 1;
-		}
-		else if (curPos.y > winSize.y)
-		{
-			player.setPos(sf::Vector2f(curPos.x, winSize.y));
-			curVel.y = 0;
-		}
-		
-		// Map collide
-		curPos = player.getPos();
-		player.update();
-		nextPos = player.getPos();
+		// Update
+		player.Move(lastDir);
+		player.update(map, time);
 
-		isCollide = map.isCollide(curPos, nextPos);
-
-		// Y bottom
-		if (isCollide.y.x) 
-		{	
-			player.setPos(sf::Vector2f(nextPos.x, int(nextPos.y / cageSize) * cageSize));
-			curVel.y = 0;
-		
-		}
-		// Y top
-		if (isCollide.y.y)
-		{
-			player.setPos(sf::Vector2f(nextPos.x, nextPos.y + curVel.y));
-			curVel.y = 0;
-		}
-		// X left
-		if (isCollide.x.x)
-		{
-			player.setPos(sf::Vector2f(int(nextPos.x / cageSize) * cageSize + player.getSize().x / 2, nextPos.y));
-		}
-		// X right
-		if (isCollide.x.y)
-		{
-			player.setPos(sf::Vector2f(int(nextPos.x / cageSize) * cageSize, nextPos.y));
-		}
 
 		// Clear
 		window.clear(sf::Color::White);
 	
+
 		// Draw block
 		mapSprite = map.getSprites();
 
--- Player.h
+++ Player.h
@@ -1,14 +1,17 @@
 #include<SFML/Graphics.hpp>
 #include<iostream>
+#include "Map.h"
 
 #pragma once
 class Player
 {
 private:
-	sf::Vector2f plPosition;
-	sf::Vector2f plVelocity = { 0, 0 };
-	sf::Vector2u plSize;
+	sf::FloatRect Rect;
+	sf::Vector2f plVelocity = { 0.0f, 0.0f };
+	const sf::Vector2f defVel = { 0.333f, 1.333f };
+	bool onGround = false;
 	sf::Texture plTexture;
+
 public:
 	Player(std::string path)
 	{
@@ -17,18 +20,25 @@ public:
 			std::cout << "Could not open player texture" << std::endl;
 		}
 
-		plSize = plTexture.getSize();
+		sf::Vector2u size = plTexture.getSize();
+
+		Rect.width = size.x;
+		Rect.height = size.y;
 	}
 
-	void update();
+	void update(Map map, float time);
+
+	void Collision(Map map, int mode);
+
+	void Move(int direction);
+
+	void Jump();
 
 	void setPos(sf::Vector2f pos);
 
 	sf::Vector2f getPos();
 
-	void setVel(sf::Vector2f vel);
-
-	sf::Vector2f getVel();
+	bool getStatus();
 
 	sf::Vector2u getSize();
 
