d9774be1aa937de28807afb3e2073d07ce05951c
Ismaxis <83420512+Ismaxis@users.noreply.github.com>
2021-08-17 18:24:09 +0300

    Auto win size similar with device prop

--- Main.cpp
+++ Main.cpp
@@ -14,22 +14,25 @@ int main()
 	// Sizes
 	const float tileSize = map.getCageSize();
 	const sf::Vector2u gridTileSize = map.getGridSize();
-	const sf::Vector2f winTileSize = { 60,  34 };
-	
 	const sf::Vector2f mapPixelSize{ gridTileSize.x * tileSize, gridTileSize.y * tileSize };
-	const sf::Vector2f winPixelSize{ winTileSize.x * tileSize, winTileSize.y * tileSize };
+	
+	const sf::Vector2f winPixelSize{ float(sf::VideoMode::getDesktopMode().width - 1), float(sf::VideoMode::getDesktopMode().height - 1) };
+	const sf::Vector2f winTileSize = { winPixelSize.x/tileSize,  round(winPixelSize.y/tileSize) };
+	
+	//const sf::Vector2f winPixelSize{ winTileSize.x * tileSize, winTileSize.y * tileSize };
 	
 	// Window
 	sf::RenderWindow window(sf::VideoMode(winPixelSize.x, winPixelSize.y), "Game", sf::Style::None);
 	window.setFramerateLimit(144);
-	window.setPosition(sf::Vector2i(350, 150));
+	//window.setPosition(sf::Vector2i(350, 150));
+	//window.setSize(sf::Vector2u(2 * winPixelSize.x / 3, 2 * winPixelSize.y / 3));
 	
 	// Player
-	Player player("Textures/Player64Õ128.png");
+	Player player("Textures/Player64X128.png");
 	player.setPos(sf::Vector2f(200.0f,  mapPixelSize.y / 2.0f));
 
 	// Variables
-	sf::Vector2f offset {0, 0};
+	sf::Vector2f offset{ 0, 0 };
 	sf::Clock clock;
 	int direction{ 0 };
 	
@@ -42,7 +45,7 @@ int main()
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
-			
+
 			if (event.type == sf::Event::Closed)
 			{
 				window.close();
@@ -52,7 +55,7 @@ int main()
 			{
 				std::cout << std::endl;
 			}
-			
+
 			if (event.type == sf::Event::KeyPressed)
 			{
 				// X movement
@@ -64,7 +67,7 @@ int main()
 				{
 					direction = -1;
 				}
-				
+
 				// Jump 
 				if ((event.key.code == sf::Keyboard::Space || event.key.code == sf::Keyboard::W) && player.getStatus())
 				{
@@ -96,17 +99,17 @@ int main()
 			offset.x = 0;
 		}
 
-		
+
 		// Clear
 		window.clear(sf::Color::White);
-	
+
 
 		// Draw block
+		const sf::Vector2f offsetInCages{ offset.x / tileSize, offset.y / tileSize };
 		for (float i = 0; i < winTileSize.y; i++)
 		{
 			for (float j = 0; j < winTileSize.x + 1; j++)
-			{ 
-				const sf::Vector2f offsetInCages{ offset.x / tileSize, offset.y / tileSize};
+			{
 				window.draw(map.getSprite(sf::Vector2u(j + offsetInCages.x, i + offsetInCages.y), offset));
 			}
 		}
@@ -114,5 +117,4 @@ int main()
 		window.draw(player.getSprite(offset));
 		window.display();
 	}
-	system("exit");
 }
--- Map.h
+++ Map.h
@@ -66,7 +66,5 @@ public:
 
 	int getCage(sf::Vector2i coords);
 
-	std::vector<std::vector<sf::Sprite>> getSprites(sf::Vector2f offset);
-
 	sf::Sprite getSprite(sf::Vector2u pos, sf::Vector2f offset);
 };
